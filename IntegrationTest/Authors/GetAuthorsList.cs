5b8d97c92883862e893a742463bea1dfbd2d34b9
AmrElshaer <amrelsher07@gmail.com>
2023-07-29 16:42:30 +0300

    update

--- IntegrationTest/Authors/GetAuthorsList.cs
+++ IntegrationTest/Authors/GetAuthorsList.cs
@@ -8,15 +8,15 @@ namespace IntegrationTest.Authors;
 public class GetAuthorsList : BaseTestFixture
 {
     [Test]
-    public async Task GetAuthors_EmptyAuthors_SuccessOK()
+    public async Task GetAuthors_SuccessOK()
     {
         // Act
         var response = await Client.GetAsync("/api/author");
         var stringResponse = await response.Content.ReadAsStringAsync();
         var model = stringResponse.FromJson<AuthorDto[]>();
         // Assert
-        model.Should().NotBeNull();
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+        model.Should().NotBeNull();
         model.Should().BeOfType<AuthorDto[]>();
     }
 }
